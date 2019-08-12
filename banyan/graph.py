40a0975c5accc832ba71c75b735300ae8b7498ed
Robert Daland <rdaland@Roberts-MacBook-Air.local>
2019-08-11 22:13:04 -0700

    installed and lightly tested networkx -- adequate for present purposes

--- banyan/graph.py
+++ banyan/graph.py
@@ -1,1 +1,6 @@
 
+class Graph(dict):
+    def __init__(self):
+        pass
+
+    def layout
--- dev-data/make-dat.py
+++ dev-data/make-dat.py
@@ -1,7 +1,12 @@
 import json
 import logging
+import networkx
 
 logging.basicConfig(level=logging.INFO)
+
+#################
+# JSON-style
+#################
 
 DATA_FILE = "banyan1.json"
 
@@ -26,5 +31,41 @@ logging.info('created node')
 with open(DATA_FILE, 'w') as fout:
     fout.write(json.dumps(x, indent=2))
 logging.info('wrote to file ' + DATA_FILE)
+
+#################
+# networkx style
+#################
+
+# create directed graph
+dev_graph = networkx.DiGraph()
+
+# create daughters of root by adding edges from 'root' to 'root.<domain>'
+for domain in ['phone', 'sms', 'media', 'encyclopedia', 'translation']:
+    dev_graph.add_edge('root', 'root.'+domain, name=domain)
+# make tree have depth 2 to force recursion and avoid flatness-errors
+for verb in ['call', 'callback']:
+    dev_graph.add_edge('root.phone', 'root.phone.'+verb, name=verb)
+
+# assign 'count' attribute to terminals
+ct_map = {
+    'root.phone.call': 29,
+    'root.phone.callback': 1,
+    'root.sms': 25,
+    'root.media': 150,
+    'root.encyclopedia': 20,
+    'root.translation': 10
+}
+for nodename, node_ct in ct_map.items():
+    dev_graph.nodes[nodename]['count'] = node_ct
+
+# aggregate 'count' attribute from terminals under 'root.phone'
+dev_graph.nodes['root.phone']['count'] = sum([dev_graph.nodes[node]['count']
+            for node in networkx.descendants(dev_graph, 'root.phone')])
+# ^ only works properly because all children of 'root.phone' are terminals
+# to do same for 'root', need to restrict to children of 'root'
+dev_graph.nodes['root']['count'] = sum([dev_graph.nodes[node]['count']
+            for node in dev_graph.succ['root']])
+
+
 
 
